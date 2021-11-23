619798888a6d8139244eeb0c26678170e6c0650b
mostafagamalsaidibrahi <mycvSiteByEngMostafa@gmail.com>
2021-11-23 20:43:03 +0200

    busicol Template

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+  'use strict';
+  // var ScreenHeight = $(window).innerHeight();
+  // $('.main-div').height(ScreenHeight/2.6);
+  // $('.main-div .cover').height(ScreenHeight/2.6);
+  //$('.problem-solving').height(ScreenHeight/5);
+ // $('.problem-solving .mainImage').height(ScreenHeight/5);
+  //$('.problem-solving .mainImage .content-image').height(ScreenHeight/5);
+ // $('.problem-solving .mainImage .content-image .content .row').height(ScreenHeight/5);
+});
